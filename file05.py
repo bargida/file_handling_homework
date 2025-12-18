9e27b30242393564c69fb4546bc347def6701f38
bargida <bargida.tilyakova25@gmail.com>
2025-12-18 11:27:42 +0500

    task is done

--- file05.py
+++ file05.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    digit =0
+    non_digit =0
+    for num in data:
+        if num.isdigit():
+            digit+=1
+        else:
+            non_digit+=1
+    return [digit, non_digit]
 # Read data from file
+data = open('./data/data05.txt').read()
+print(main(data))
