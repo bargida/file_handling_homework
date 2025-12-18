641c86c650ac58430b441d75a9be785090b48e55
bargida <bargida.tilyakova25@gmail.com>
2025-12-18 13:45:20 +0500

    task is done

--- file10.py
+++ file10.py
@@ -6,5 +6,18 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    # large_row = 0
+    # for i in data:
+    #     if len(i)>large_row:
+    #         large_row = len(i)
+    # return large_row
+    large_char = data[0]
+
+    for i in range(len(data)):
+        if len(data[i]) > len(large_char):
+            large_char = data[i]
+    return large_char[:-1]
 
 # Read data from file
+df = open('./data/data10.txt').readlines()
+print(main(df))
