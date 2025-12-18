7f569f37fa32561a2dc7fe2cddd2c437f94d1cda
bargida <bargida.tilyakova25@gmail.com>
2025-12-18 13:05:57 +0500

    task is done

--- file08.py
+++ file08.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    max_value=-1
+    for i in data:
+        if i.isdigit():
+            if int(i)>max_value:
+                max_value=int(i)
+    return max_value
 
 # Read data from file
+df = open('./data/data08.txt').read()
+print(main(df))
